72623b6e36aef1c055e6b87c8705cdb51f7bb403
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-03-01 10:01:30 +0530

    api changes

--- pages/getAPI.js
+++ pages/getAPI.js
@@ -16,7 +16,9 @@ const buyapi = () => {
         strategy="beforeInteractive"
         src="https://cdn.tailwindcss.com"
       ></Script>
-      
+      <Link href="/api/blogs">
+        <h1 className="bg-pink-200 text-emerald-500 text-center p-4 hover:bg-pink-500 hover:text-emerald-800 mx-auto w-[80rem] m-10 cursor-pointer">Free API</h1>
+      </Link>
       <iframe src="/api/blogs" className="w-[50rem] h-[30rem] mx-auto border-lime-600 border-4 "></iframe>
     </div>
   );
